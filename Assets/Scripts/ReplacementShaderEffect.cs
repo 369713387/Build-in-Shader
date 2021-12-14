c21501f907b07061b2d5711b50f183400a14e5e1
369713387 <369713387@qq.com>
2021-12-14 18:24:24 +0800

    新增一个shader

---
+++ Assets/Scripts/ReplacementShaderEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ReplacementShaderEffect : MonoBehaviour
+{
+    public Shader ReplacementShader;
+
+    private void OnEnable()
+    {
+            GetComponent<Camera>().SetReplacementShader(ReplacementShader, "RenderType");
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<Camera>().ResetReplacementShader();
+    }
+}
